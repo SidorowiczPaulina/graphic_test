7983e7e90c49ace67b3f1a7bfa7acf7180701525
Paulina Sidorowicz <p.kondraciuk95@gmail.com>
2023-11-26 15:13:05 +0100

    Actual version

--- Shift_Planer/viewer/urls.py
+++ Shift_Planer/viewer/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout', views.user_logout, name='logout'),
-    path('schedule/', views.create_schedule, name='create_schedule'),
+    path('schedule/', views.create_schedule, name='create_schedule', ),
     path('enter_availability/', views.enter_availability, name='enter_availability'),
     path('home/', views.home, name='home'),
 ]
